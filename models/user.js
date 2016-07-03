23f407df355e2c256e7572e8d3680b0f550d5b53
lnvtphu <lnvtphu@gmail.com>
2016-07-04 01:30:01 +0700

    4/7

--- models/user.js
+++ models/user.js
@@ -2,20 +2,102 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 var connectdata = require('../connect_data');
 mongoose = connectdata.initialize(mongoose);
-var User = new Schema({
+var UserSchema = new Schema({
     "_id": String,
     "firstName": String,
     "lastName": String,
     "photo": String,
     "friends": []
 });
-var UserModel = mongoose.model('User', User);
+var User = mongoose.model('User', UserSchema);
 module.exports = {
       viewall: function(req, res){
-          var usermodel = UserModel.find({});
-          usermodel.exec(function(err, st){
-              console.log(st);
-              res.json(st);
+          console.log("viewall");
+          var usermodel = User.find({}).sort({_id: 1});
+          usermodel.exec(function(err, data){
+              res.status(200).json(data);
           });
-      }
+      },
+      adduser: function(req, res){
+          var idUser = req.body._id;
+          if(!idUser){
+              res.status(404).json({Error: '_id null'});
+              return;
+          }
+          User.count({_id: idUser}, function(err, count){
+             if(err){
+                  res.status(404).json(err);
+             } else if(count == 0){
+                 var user = new User (req.body);
+                 user.save(function(err){
+                     console.log(err);
+                     res.status(200).json({Sucess: 'Create success'});
+               });
+             }else{
+                 res.status(404).json({Error: 'User exist'});
+             }
+        });
+    },
+    deleteuser: function(req, res){
+        var id = req.body._id;
+        User.findOneAndRemove({_id:id}, function(err,data){
+            if(err){
+                res.status(404).json(err);
+            }else if(!data){
+                res.status(404).json({Error: 'User with ID: ' + id + 'not exist on data'});
+            }else{
+                res.status(200).json({Success:'Delete success'});
+                }
+            }
+        );
+    },
+    updateuser: function(req, res){
+          var reqUser = req.body;
+          var id = req.body._id;
+
+          if(!id){
+              res.status(400).json({Error: 'Id null'});
+              return;
+          }
+          User.findOneAndUpdate(
+              {_id: id},
+              {
+                $set:{
+                    firstName: reqUser.firstName,
+                    lastName: reqUser.lastName,
+                    photo: reqUser.photo
+                }
+            },
+              function(err, data){
+                  if(err){
+                      res.status(404).json(err);
+                  }else{
+                      res.status(200).json({Success:'Update success'});
+                  }
+              }
+          );
+    },
+    deletefriend: function(req, res){
+        var id = req.body._id;
+        var friendsArr = req.body.friends;
+        if(!id){
+            res.status(400).json({Error: 'Id null'});
+            return;
+        }
+        User.findOneAndUpdate(
+            {_id: id},
+            {
+              $set:{
+                  friends: friendsArr
+              }
+          },
+            function(err, data){
+                if(err){
+                    res.status(404).json(err);
+                }else{
+                    res.status(200).json({Success:'Update success'});
+                }
+            }
+        );
+    }
 }
